056535c54832446c78e5af61fbe4f334a8e58a29
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-02-23 14:26:20 +0300

    Added tests of calaculating a determinant in some corner cases

---
+++ tests/determinant_tests.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "../Matrix_Lib/include/matrix.hpp"
+#include "../Matrix_Lib/include/comparison.hpp"
+
+using namespace cmp;
+using namespace Matrix_Algebra;
+
+TEST (DETERMINANT_TESTS, FINDING_PIVOT)
+{
+    Matrix<float> A = {{0.0, 1.0},
+                       {1.0, 0.0}};
+    EXPECT_TRUE (are_equal(A.determinant(), -1.0, epsilon));
+}
+
+TEST (DETERMINANT_TESTS, ZERO_DET)
+{
+    Matrix<float> B = {{0.0, 1.0, 2.0},
+                       {0.0, 3.0, 4.0},
+                       {0.0, 5.0, 6.0}};
+    EXPECT_TRUE (are_equal(B.determinant(), 0.0, epsilon));
+}
+
+int main (int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
