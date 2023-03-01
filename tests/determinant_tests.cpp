8f60f6e9ec669e332d2c84364080ae460a35533d
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-03-01 23:34:04 +0300

    Added 1 test of calling determinant() for integral matrix

--- tests/determinant_tests.cpp
+++ tests/determinant_tests.cpp
@@ -9,15 +9,22 @@ TEST (DETERMINANT_TESTS, FINDING_PIVOT)
 {
     Matrix<float> A = {{0.0, 1.0},
                        {1.0, 0.0}};
-    EXPECT_TRUE (are_equal(A.determinant(), -1.0, epsilon));
+    EXPECT_TRUE(are_equal(A.determinant(), -1.0, epsilon));
 }
 
 TEST (DETERMINANT_TESTS, ZERO_DET)
 {
-    Matrix<float> B = {{0.0, 1.0, 2.0},
+    Matrix<float> A = {{0.0, 1.0, 2.0},
                        {0.0, 3.0, 4.0},
                        {0.0, 5.0, 6.0}};
-    EXPECT_TRUE (are_equal(B.determinant(), 0.0, epsilon));
+    EXPECT_TRUE(are_equal(A.determinant(), 0.0, epsilon));
+}
+
+TEST (DETERMINANT_TESTS, CALL_FOR_INTEGER)
+{
+    Matrix<int> A = {{0, 1}, 
+                     {1, 0}};
+    EXPECT_TRUE(A.determinant() == -1);
 }
 
 int main (int argc, char** argv)
