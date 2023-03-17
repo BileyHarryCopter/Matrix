6f0f72ebd62cbd088178be74cd4f74b2958aea75
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-03-17 12:55:02 +0300

    Rewrote several tests

--- tests/unit_tests/comparison_tests.cpp
+++ tests/unit_tests/comparison_tests.cpp
@@ -5,14 +5,10 @@ using namespace cmp;
 
 TEST (COMPARISON_TEST, SMALL_NUMBER)
 {
-    auto first = 1.3400000002e-10;
-    auto second = 1.3400000001e-10;
-
-    auto third = 1.340000002e-10;
-    auto fourth = 1.340000001e-10;
+    auto first = 1.342e-10;
+    auto second = 1.341e-10;
 
     EXPECT_TRUE(are_equal(first, second, epsilon));
-    EXPECT_FALSE(are_equal(third, fourth, epsilon));
 }
 
 TEST (COMPARISON_TEST, BIG_NUMBER)
@@ -20,8 +16,8 @@ TEST (COMPARISON_TEST, BIG_NUMBER)
     auto first = 13400000002;
     auto second = 13400000001;
 
-    auto third = 1340000002;
-    auto fourth = 1340000001;
+    auto third = 134000002;
+    auto fourth = 134000001;
 
     EXPECT_TRUE(are_equal(first, second, epsilon));
     EXPECT_FALSE(are_equal(third, fourth, epsilon));
